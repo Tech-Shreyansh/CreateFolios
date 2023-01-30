580dc209cb34f6d5047437e3237b0b80545534e0
Shreyansh Agarwal <shreyanshagarwal.16.2022@gmail.com>
2023-01-30 14:26:56 +0530

    added profile portion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
 import PortFolio from "./PortfolioTemplate/portFolio";
 
 function App() {
   return <BrowserRouter >
   <Routes>
-    <Route path='/home'></Route>
+    <Route path='/'></Route>
     <Route path='/template-1' element={<PortFolio />} ></Route>
   </Routes>
   </BrowserRouter>
--- src/PortfolioTemplate/portFolio.jsx
+++ src/PortfolioTemplate/portFolio.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import './portfolio.css'
+import profile from './images/mypic.jpeg'
 
 const PortFolio = () => {
     return <div>
-        hiii Shreya
+        <div id="home">
+            <div>
+                <marquee scrollamount="10">
+                    <h4>What's Up Friend🖐🏻, Myself</h4>
+                </marquee>
+            </div>
+            <h3>SHREYANSH AGARWAL</h3>
+            <img src={profile} alt="why can 't yo see me?" />
+            <p>
+            I don't believe in finding myself👀 instead I believe in creating myself😎!! </p>
+        </div>
     </div>
 }
 
